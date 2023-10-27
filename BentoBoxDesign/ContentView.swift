a2fbca66123df6763b2159d4ecd9fae0991d5f4a
Russell Gordon <rgordon@lcs.on.ca>
2023-10-27 18:02:57 -0400

    Got colours to largely match the target; also gradients for middle text.

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -8,27 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let lightGray = Color(
+        hue: 0/360.0,
+        saturation: 0.0,
+        brightness: 0.9
+    )
+    let mediumGray = Color(
+        hue: 0/360.0,
+        saturation: 0.0,
+        brightness: 0.8
+    )
+    let darkGray = Color(
+        hue: 0/360.0,
+        saturation: 0.0,
+        brightness: 0.45
+    )
+    let lightPurple = Color(
+        hue: 266.0/360.0,
+        saturation: 0.0,
+        brightness: 1.0
+    )
+    let mediumPurple = Color(
+        hue: 266.0/360.0,
+        saturation: 0.35,
+        brightness: 0.50
+    )
+    let darkPurple = Color(
+        hue: 286.0/360.0,
+        saturation: 0.65,
+        brightness: 0.50
+    )
+
     var body: some View {
         VStack {
             HStack {
                 
                 // Left column
                 Rectangle()
-                    .fill(.red)
-                
+                    .fill(mediumGray)
+                    .cornerRadius(10.0)
+
                 // Middle column
                 VStack {
                     
                     // Top row
                     Rectangle()
-                        .fill(.gray)
+                        .fill(mediumGray)
+                        .cornerRadius(10.0)
                         .frame(height: 100)
                     
                     // Middle row
                     ZStack {
                         Rectangle()
-                            .fill(.gray)
-                        
+                            .fill(mediumGray)
+                            .cornerRadius(10.0)
+
                         // Chip
                         ZStack {
                             Rectangle()
@@ -40,21 +75,28 @@ struct ContentView: View {
                                         .font(.custom("SF Pro Medium", size: 48.0, relativeTo: .largeTitle))
                                         .foregroundStyle(
                                             LinearGradient(
-                                                colors: [
-                                                    Color(hue: 0, saturation: 0, brightness: 0.4),
-                                                    Color(hue: 0, saturation: 0, brightness: 1.0)
-                                                ],
+                                                colors: [darkGray, mediumGray],
                                                 startPoint: .bottomLeading,
                                                 endPoint: .topTrailing
                                             )
                                         )
                                     Text("M2")
                                         .font(.custom("SF Pro Medium", size: 38.0, relativeTo: .largeTitle))
-                                        .foregroundStyle(Color(hue: 0, saturation: 0, brightness: 0.8))
+                                        .foregroundStyle(
+                                            LinearGradient(
+                                                colors: [mediumGray, lightGray],
+                                                startPoint: .bottomLeading,
+                                                endPoint: .topTrailing
+                                            )
+                                        )
                                 }
 
+                                
                                 Text("MAX")
                                     .font(.custom("SF Pro Medium", size: 22.0, relativeTo: .largeTitle))
+                                    .foregroundStyle(
+                                        RadialGradient(colors: [lightPurple, darkPurple], center: .bottomLeading, startRadius: 0, endRadius: 45)
+                                    )
                             }
                             .foregroundStyle(.white)
                         }
@@ -64,17 +106,22 @@ struct ContentView: View {
                     
                     // Bottom row
                     Rectangle()
-                        .fill(.gray)
+                        .fill(mediumGray)
+                        .cornerRadius(10.0)
                         .frame(height: 75)
                 }
                 
                 // Right column
                 Rectangle()
-                    .fill(.green)
+                    .fill(mediumGray)
+                    .cornerRadius(10.0)
 
             }
         }
+        .padding(10)
+        .background(.white)
         .frame(width: 800, height: 400)
+        .cornerRadius(5.0)
         .padding()
     }
 }
