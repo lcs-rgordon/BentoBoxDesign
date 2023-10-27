c2234355d5812e6a03275b49789b0da648216449
Russell Gordon <rgordon@lcs.on.ca>
2023-10-27 11:48:23 -0400

    Chose a better icon and made it large enough to see.

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image(systemName: "lasso.badge.sparkles")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 125)
             Text("Hello, world!")
         }
         .padding()
